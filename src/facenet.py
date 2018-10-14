60477822818d8d1a22826722ef4e60eff306e0ca
ShuaiWang0410 <shuaiwang0410@gmail.com>
2018-10-14 00:21:34 -0400

    First-Try

--- src/facenet.py
+++ src/facenet.py
@@ -34,10 +34,10 @@ import numpy as np
 from scipy import misc
 from sklearn.model_selection import KFold
 from scipy import interpolate
-# from tensorflow.python.training import training # ShuaiWang: in order to make use of tf-gpu
+from tensorflow.python.training import training # ShuaiWang: in order to make use of tf-gpu
 import random
 import re
-# from tensorflow.python.platform import gfile #
+from tensorflow.python.platform import gfile #
 import math
 from six import iteritems
 
@@ -365,26 +365,24 @@ def load_model(model, input_map=None):
     # Check if the model is a model directory (containing a metagraph and a checkpoint file)
     #  or if it is a protobuf file with a frozen graph
     model_exp = os.path.expanduser(model)
-    '''
+
     
-    ShuaiWang: not included in tensorflow-gpu
+    # ShuaiWang: not included in tensorflow-gpu
     if (os.path.isfile(model_exp)):
         print('Model filename: %s' % model_exp)
         with gfile.FastGFile(model_exp,'rb') as f:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(f.read())
             tf.import_graph_def(graph_def, input_map=input_map, name='')
-    
     else:
-    '''
-    print('Model directory: %s' % model_exp)
-    meta_file, ckpt_file = get_model_filenames(model_exp)
+        print('Model directory: %s' % model_exp)
+        meta_file, ckpt_file = get_model_filenames(model_exp)
 
-    print('Metagraph file: %s' % meta_file)
-    print('Checkpoint file: %s' % ckpt_file)
+        print('Metagraph file: %s' % meta_file)
+        print('Checkpoint file: %s' % ckpt_file)
 
-    saver = tf.train.import_meta_graph(os.path.join(model_exp, meta_file), input_map=input_map)
-    saver.restore(tf.get_default_session(), os.path.join(model_exp, ckpt_file))
+        saver = tf.train.import_meta_graph(os.path.join(model_exp, meta_file), input_map=input_map)
+        saver.restore(tf.get_default_session(), os.path.join(model_exp, ckpt_file))
     
 def get_model_filenames(model_dir):
     files = os.listdir(model_dir)
@@ -546,7 +544,7 @@ def store_revision_info(src_path, output_dir, arg_string):
         text_file.write('tensorflow version: %s\n--------------------\n' % tf.__version__)  # @UndefinedVariable
         text_file.write('git hash: %s\n--------------------\n' % git_hash)
         text_file.write('%s' % git_diff)
-'''
+
 # ShuaiWang no use for this, and tensorflow-gpu doesn't support this.
 
 def list_variables(filename):
@@ -554,7 +552,7 @@ def list_variables(filename):
     variable_map = reader.get_variable_to_shape_map()
     names = sorted(variable_map.keys())
     return names
-'''
+
 def put_images_on_grid(images, shape=(16,8)):
     nrof_images = images.shape[0]
     img_size = images.shape[1]
